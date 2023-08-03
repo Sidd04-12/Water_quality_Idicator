d358ac53cd7654358fdd9379d82bdf29b591c841
Sidd04-12 <115709060+Sidd04-12@users.noreply.github.com>
2023-08-04 01:00:57 +0530

    Prediction succesfull

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 const express = require('express');
 const app = express();
-const path =require('path');
+const path = require('path');
+const { spawn } = require('child_process');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/form',(req,res)=>{
-    res.render('form');
-})
+app.get('/form', (req, res) => {
+  res.render('form');
+});
 
+app.post('/predict', (req, res) => {
+  const data = req.body;
 
+  const pythonProcess = spawn('python', ['python_scripts/model_train.py', JSON.stringify(data)]);
 
-app.listen(3000, ()=>{
-    console.log("On port 3000");
-})
+  let result = '';
+  pythonProcess.stdout.on('data', (data) => {
+    result += data.toString();
+  });
+
+  pythonProcess.stderr.on('data', (data) => {
+    console.error('Error:', data.toString());
+    res.status(500).json({ error: 'An error occurred. Please try again later.' });
+  });
+
+  pythonProcess.on('close', (code) => {
+    try {
+      const prediction = JSON.parse(result).prediction;
+      const message = prediction === 1 ? 'The water is potable.' : 'The water is not potable.';
+      res.json({ prediction, message });
+    } catch (err) {
+      console.error('Error:', err);
+      res.status(500).json({ error: 'An error occurred. Please try again later.' });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
--- public/script.js
+++ public/script.js
@@ -1,26 +1,24 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
-    const waterQualityForm = document.getElementById('waterQualityForm');
-    const resultDiv = document.getElementById('result');
-  
-    waterQualityForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-  
-      const formData = new FormData(waterQualityForm);
-  
-      const data = {};
-      formData.forEach((value, key) => {
-        data[key] = value;
-      });
-  
-      fetch('/predict', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
+  const waterQualityForm = document.getElementById('waterQualityForm');
+  const resultDiv = document.getElementById('result');
+
+  waterQualityForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(waterQualityForm);
+
+    const data = {};
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+
+    fetch('/predict', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
       .then(response => response.json())
       .then(result => {
         const prediction = result.prediction;
@@ -31,6 +29,5 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error('Error:', error);
         resultDiv.textContent = 'An error occurred. Please try again later.';
       });
-    });
   });
-  
+});
